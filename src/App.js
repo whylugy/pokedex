7c43719013a203711eab2d642000cf2ab9d75e10
Luqman A <whylugy.beats@gmail.com>
2021-12-25 16:53:35 +0000

    Hello World
    changed the intro in the app.js to say Hello World!

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, World! My name is Luqman!
         </p>
+        <p>;)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
